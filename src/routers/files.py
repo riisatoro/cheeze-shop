abd304d2f302d5a2c9ec2f91b151d271334ede9f
Daniil. I <riisa.toro@gmail.com>
2023-12-08 03:00:42 +0200

    Files upload feature

--- src/routers/files.py
+++ src/routers/files.py
@@ -1,4 +1,9 @@
-from fastapi import APIRouter, File, UploadFile
+import os
+from typing import List
+from fastapi import APIRouter, UploadFile, Depends
+
+from database.dependencies import get_user_from_token
+from schemas import UserProfile, DefaultResponse
 
 
 router = APIRouter(
@@ -7,4 +12,27 @@ router = APIRouter(
 )
 
 
+@router.get("/")
+async def get_dir_content(
+    directory: str | None = None,
+    user: UserProfile = Depends(get_user_from_token),
+):
+    ...
 
+
+@router.post("/")
+async def upload_files(
+    files: List[UploadFile],
+    directory: str | None = None,
+    user: UserProfile = Depends(get_user_from_token),
+) -> DefaultResponse:
+
+    user_dir = os.path.join('media', user.folder_hash, directory or "")
+    os.mkdir(user_dir) if not os.path.exists(user_dir) else None
+
+    for file in files:
+        file_path = os.path.join(user_dir, file.filename)
+        with open(file_path, "wb") as storage_file:
+            storage_file.write(await file.read())
+
+    return DefaultResponse(message="Files uploaded successfully")
