f3eb06350c9f44074deb72083288a8fe15e2ff59
Daniil. I <riisa.toro@gmail.com>
2023-12-08 03:10:48 +0200

    Basic files and folders structure

--- src/routers/files.py
+++ src/routers/files.py
@@ -1,9 +1,9 @@
 import os
 from typing import List
-from fastapi import APIRouter, UploadFile, Depends
+from fastapi import APIRouter, UploadFile, Depends, HTTPException
 
 from database.dependencies import get_user_from_token
-from schemas import UserProfile, DefaultResponse
+from schemas import UserProfile, DefaultResponse, FolderContent, FileInfo
 
 
 router = APIRouter(
@@ -16,8 +16,22 @@ router = APIRouter(
 async def get_dir_content(
     directory: str | None = None,
     user: UserProfile = Depends(get_user_from_token),
-):
-    ...
+) -> FolderContent:
+    user_dir = os.path.join('media', user.folder_hash, directory or "")
+    if not os.path.exists(user_dir):
+        if directory:
+            raise HTTPException(status_code=404, detail='Folder not found')
+        os.mkdir(user_dir)
+
+    response = FolderContent(folders=[], files=[])
+    for file_or_folder in os.scandir(user_dir):
+        if file_or_folder.is_dir():
+            response.folders.append(file_or_folder.name)
+        else:
+            response.files.append(FileInfo(name=file_or_folder.name))
+
+    return response
+
 
 
 @router.post("/")
